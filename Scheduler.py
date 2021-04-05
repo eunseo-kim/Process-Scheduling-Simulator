f3c0c9b48c18819a3178adca0592b060955284ff
Hyunjune Lee <44167177+hyunjune-lee@users.noreply.github.com>
2021-04-05 17:55:19 +0900

    기능 추가: RR 기본 생성자 추가, Scheduler 속성 복수형으로 변경

--- Scheduler.py
+++ Scheduler.py
@@ -6,18 +6,18 @@ from CPU import *
 class Scheduler(metaclass=ABCMeta):
     def __init__(self, process_input_list, cpu_count):
         self.process_count = len(process_input_list)
-        self.process = self.create_processes(self.process_count, process_input_list)
+        self.processes = self.create_processes(process_input_list)
         self.cpu_count = cpu_count
-        self.cpu = self.create_cpu(self.cpu_count)
+        self.cpus = self.create_cpus(self.cpu_count)
         self.ready_queue = []
 
-    def create_process(self, process_input_list):
+    def create_processes(self, process_input_list):
         process_list = []
-        for i, p in enumerate(process_input_list):
-            process[i].append(Process(p[0], p[1], p[2]))
+        for p in process_input_list:
+            process_list.append(Process(p[0], p[1], p[2]))
         return process_list
 
-    def create_cpu(self, cpu_count):
+    def create_cpus(self, cpu_count):
         cpu_list = []
         for i in range(cpu_count):
             cpu_list.append(CPU(i + 1))
