0c4841af8ca0ae3ac868f99ffb29c2b0bb664f11
testihrx <81636372+testihrx@users.noreply.github.com>
2021-04-07 00:26:50 +0900

    임시적으로 GUI 구현중
    > 첫 줄에서 알고리즘 종류, 프로세스 이름, AT, BT를 받고 추가 / 리셋
    > 두 번째 줄에 받은 프로세스 리스트를 보여주는 표와 프로세서 개수, Qunaturn 선정하는거

--- show.py
+++ show.py
@@ -9,11 +9,10 @@ class MyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.Proc_List = []
+        self.Run_Start = True
         self.initUI()
 
     def initUI(self):
-        # 첫째줄, 그리드
-        grid_Line1 = QGridLayout()
 
         self.Alg_Select = QComboBox(self)
         self.Alg_Select.addItem('FCFS')
@@ -22,6 +21,7 @@ class MyApp(QWidget):
         self.Alg_Select.addItem('SRTN')
         self.Alg_Select.addItem('HRRN')
         self.Alg_Select.addItem('YOA')
+        self.Alg_Select.activated.connect(self.enableSlot)
 
         ProName = QLineEdit()
         AT = QSpinBox()
@@ -39,8 +39,25 @@ class MyApp(QWidget):
         addButton.clicked.connect(lambda: self.add(ProName.text(),AT.value(),BT.value()))
         resetButton = QPushButton('Reset', self)
         resetButton.clicked.connect(self.reset)
-        
-        #첫째줄 그리드에 프로세스 이름, AT, BT, 추가버튼, 리셋버튼 추가
+
+        self.CPU_Number = QComboBox(self)
+        self.CPU_Number.addItem('1')
+        self.CPU_Number.addItem('2')
+        self.CPU_Number.addItem('4')
+
+        # quantum을 넣는 스핀박스, 초기는 FCFS이기에 비활성화해둠
+        self.TQ = QSpinBox()
+        self.TQ.setRange(0, 65535)
+        self.TQ.setDisabled(True)
+
+        # Run 알고리즘은 크게 실행도중인 경우 / 아닌경우, 아닌경우에서 Alg_Select가 어떤 알고리즘을 골랐느냐에 따라 선정이 달라질거임.
+        # Run과 Stop은 최하단에서 선언해서 실행하도록 해야할것..아마?
+        Run_Alg = QPushButton('Run', self)
+        resetButton.clicked.connect(self.Run_Algorithm)
+        Stop_Alg = QPushButton('Stop', self)
+
+        # 첫째줄, 그리드에 프로세스 이름, AT, BT, 추가버튼, 리셋버튼 추가
+        grid_Line1 = QGridLayout()
         grid_Line1.addWidget(QLabel('Algorithm'), 0, 0)
         grid_Line1.addWidget(QLabel('Process Name'), 0, 1)
         grid_Line1.addWidget(QLabel('AT'), 0, 2)
@@ -54,25 +71,12 @@ class MyApp(QWidget):
 
         #두번째 줄 그리드, 모양만 구현하고 실제 실행은 되는거 없음
         grid_Line2 = QGridLayout()
-
-        self.CPU_Number = QComboBox(self)
-        self.CPU_Number.addItem('1')
-        self.CPU_Number.addItem('2')
-        self.CPU_Number.addItem('4')
-
-        self.TQ = QSpinBox()
-        self.TQ.setRange(0, 65535)
-
-        Run_Alg = QPushButton('Run', self)
-        Stop_Alg = QPushButton('Stop', self)
         grid_Line2.addWidget(QLabel('Process Number'),0,0)
         grid_Line2.addWidget(self.CPU_Number, 0, 1)
         grid_Line2.addWidget(QLabel('Time qunaturn'),1,0)
         grid_Line2.addWidget(self.TQ, 1, 1)
         grid_Line2.addWidget(Run_Alg,2,0)
         grid_Line2.addWidget(Stop_Alg,2,1)
-        
-
 
         hbox_Line2 = QHBoxLayout()
         hbox_Line2.addWidget(self.Proc_Table)
@@ -94,6 +98,11 @@ class MyApp(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+    def enableSlot(self):
+        if self.Alg_Select.currentText() == 'RR' or self.Alg_Select.currentText() == 'SRTN':
+            self.TQ.setEnabled(True)
+        else:
+            self.TQ.setDisabled(True)
     def add(self, name, at, bt):
         # Proc_List에 프로세스 이름, AT, BT 저장
         self.Proc_List.append((name,at,bt))
@@ -109,7 +118,7 @@ class MyApp(QWidget):
         self.Proc_Table.setRowCount(0)
         self.Proc_List.clear()
     
-    def pass_use(self):
+    def Run_Algorithm(self):
         pass
     
 
