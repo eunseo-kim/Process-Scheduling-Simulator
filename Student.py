9a1a495ba52128c122e35851189d32c241ac12a2
Hyunjune Lee <44167177+hyunjune-lee@users.noreply.github.com>
2021-04-22 01:43:40 +0900

    오류 해결: allocate_subject_to_student에서 student가 아니라 subjects에서 과목당 학점을 받는 오류 해결

--- Student.py
+++ Student.py
@@ -6,6 +6,7 @@ class Student(CPU):
     def __init__(self, student_name):  # student_name는 학생 이름, class_list는 학생이 듣는 수업
         super().__init__(student_name)
         self.subject_list = []
+        self.best_grades = [0 for _ in range(25)]
 
     def add_subject_list(self, subject):
         self.subject_list.append(subject)
--- YOSA.py
+++ YOSA.py
@@ -4,6 +4,18 @@ from itertools import permutations
 from Student import *
 from Subject import *
 
+# 24^(학생수)의 행렬을 만들고 최적값을 찾는
+# 미리 예외처리를 통해 가지치기?
+# get_best_grade 함수 사용
+# find_best_cram_case => 최적 찾는 경우
+# calculate_grade => 계산해서 값 할당해주는 함수
+# [GUI]
+# gui에 어떻게 뿌려줄지 생각
+# => 그냥 내가 best_case 찾고 그에 따라서 학생들한테 값을 배정해주면 gui에서 학생들 순회하면서 값 출력
+# 학생들한테 배정할 값 - 베스트 개인 공부시간/ 팀플시간, 이에 따른 각 과목 학점 및 최종 학점, 학생 전체의 평균 학점
+# [Student] = CPU
+# best_grades = 미리 0으로 25개 초기화
+
 
 class YOSA(Scheduler):
     def __init__(self, subject_input_list, student_count):
@@ -11,6 +23,31 @@ class YOSA(Scheduler):
         self.students = self.create_students(student_count)
         self.subjects = subject_input_list
         self.allocate_subject_to_student()
+
+    def find_best_cram_case(self):
+        cnt = 0
+        for student in self.students:
+            for solo_study_time in range(25):
+                student.best_grades[solo_study_time] = self.get_best_grade(student, solo_study_time)
+            print("[student.best_grades]", cnt)
+            cnt += 1
+            for b_i, b in enumerate(student.best_grades):
+                print(b_i, b, end="\t")
+        # 1,7,2,4,14
+        # => 학생 4명일 때
+        # 학생 1 - 팀플에 1시간 투자
+        # 학생 2 - 팀플에 7시간 투자
+        # 학생 3 - 팀플에 2시간 투자
+        # 학생 4 - 팀플에 4시간 투자
+        # 팀플 시간 - 학생 1,2,3,4의 총 투자시간
+
+        # 0,0,0,0
+        # 0,0,0,1
+        # 0,0,0,2
+        # ...
+        # 0,0,0,24
+
+        # 0,0,1,0
 
     # def create_subjects(self, subject_input_list):
     #     subject_list = []
@@ -26,12 +63,13 @@ class YOSA(Scheduler):
         return student_list
 
     def run(self):
-        for s in self.students[0].subject_list:
-            print(s.process_id)
-        # print(self.students[0].subject_list)
-        for i in range(0, 20):
-            print("[study_time -", i, "]")
-            print("best_grade", self.get_best_grade(self.students[0], i))
+        self.find_best_cram_case()
+        # for s in self.students[0].subject_list:
+        #     print(s.process_id)
+        # # print(self.students[0].subject_list)
+        # for i in range(0, 20):
+        #     print("[study_time -", i, "]")
+        #     print("best_grade", self.get_best_grade(self.students[0], i))
 
     def allocate_subject_to_student(self):
         # 과목 리스트를 돌면서
@@ -55,10 +93,10 @@ class YOSA(Scheduler):
             grade_sum = 0  # 받은 학점들의 합
             all_grade = 0  # 1학점, 2학점의 grade
             for subject_idx, subject_study_hour in enumerate(study_case):
-                score = subject_study_hour * self.subjects[subject_idx].score_per_hour
-                grade = self.convert_score_to_grade(score) * self.subjects[subject_idx].grade
+                score = subject_study_hour * student.subject_list[subject_idx].score_per_hour
+                grade = self.convert_score_to_grade(score) * student.subject_list[subject_idx].grade
                 grade_sum += grade
-                all_grade += self.subjects[subject_idx].grade
+                all_grade += student.subject_list[subject_idx].grade
             average_grade = grade_sum / all_grade
             best_grade = max(best_grade, average_grade)
 
@@ -105,9 +143,7 @@ class YOSA(Scheduler):
                 for each_case in permutations(arr, len(subject_list)):  # (24, 0, 0, 0), (0, 24, 0, 0), (0, 0, 24, 0)...
                     if each_case not in result:
                         can_append = True
-                        for subject_idx, subject in enumerate(
-                            subject_list
-                        ):  # 모든 과목 BT가 8시간이라고 가정(8시간보다 더 많이 공부할 수는 없음)
+                        for subject_idx, subject in enumerate(subject_list):
                             if each_case[subject_idx] > subject.BT:
                                 can_append = False
                                 break
